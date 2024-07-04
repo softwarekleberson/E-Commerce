86aeba142d69f5290b20ed0cca156352ea7e9b32
softwarekleberson <santossilvakleberson@gmail.com>
2024-07-04 16:37:01 -0300

    Modifição da home introdução das categorias na header da home

--- src/main/java/br/com/engenharia/projeto/ProjetoFinal/dtos/Livro/DadosDetalhamentoLivro.java
+++ src/main/java/br/com/engenharia/projeto/ProjetoFinal/dtos/Livro/DadosDetalhamentoLivro.java
@@ -3,6 +3,7 @@ package br.com.engenharia.projeto.ProjetoFinal.dtos.Livro;
 import java.math.BigDecimal;
 
 import br.com.engenharia.projeto.ProjetoFinal.entidade.livro.Autor;
+import br.com.engenharia.projeto.ProjetoFinal.entidade.livro.Categoria;
 import br.com.engenharia.projeto.ProjetoFinal.entidade.livro.Imagens;
 import br.com.engenharia.projeto.ProjetoFinal.entidade.livro.Livro;
 
@@ -12,6 +13,7 @@ public record DadosDetalhamentoLivro(
     String titulo,
     String primeiraImagem,
     String autor,
+    String categoria,
     BigDecimal preco
     
 ) {
@@ -20,6 +22,7 @@ public record DadosDetalhamentoLivro(
              livro.getTitulo(),
              livro.getImagens().stream().findFirst().map(Imagens::getUrl).orElse(null),
              livro.getAutores().stream().findFirst().map(Autor::getAutor).orElse(null),
+             livro.getCategorias().stream().findFirst().map(Categoria::getCategoria).orElse(null),
              livro.getPreco());
     }
 }
