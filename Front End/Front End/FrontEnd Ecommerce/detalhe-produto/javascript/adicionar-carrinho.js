3c9c2d75e92fa3553a6d34a40b1aa338407da154
softwarekleberson <santossilvakleberson@gmail.com>
2024-10-07 18:24:39 -0300

    Retirei precifição de detalhamento de livros

--- Front End/Front End/FrontEnd Ecommerce/detalhe-produto/javascript/adicionar-carrinho.js
+++ Front End/Front End/FrontEnd Ecommerce/detalhe-produto/javascript/adicionar-carrinho.js
@@ -1,29 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // Obtendo os parâmetros da URL
+
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get('id');
     const clienteId = urlParams.get('clienteId');
 
-    // Verificar se id e clienteId estão definidos
     if (!id || !clienteId) {
-        console.error('ID ou clienteId não estão definidos na URL.');
-        alert('Erro: ID ou clienteId não estão presentes na URL.');
         return;
     }
 
-    // Adicionar evento de clique ao botão "Adicionar ao Carrinho"
     document.getElementById('adicionar-carrinho').addEventListener('click', () => {
-        const quantidade = document.getElementById('quantidade').innerText; // Captura a quantidade
+        const quantidade = document.getElementById('quantidade').innerText; 
 
-        // Montando a URL de destino
         const url = `http://localhost:8080/pedidos/${id}/${clienteId}`;
 
-        // Dados a serem enviados no POST
         const data = {
             quantidade: quantidade
         };
 
-        // Fazendo a requisição POST
         fetch(url, {
             method: 'POST',
             headers: {
