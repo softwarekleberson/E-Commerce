b23ff01d1b14e7ae902d1330fe7b61ac86ec7ae0
softwarekleberson <santossilvakleberson@gmail.com>
2024-10-17 15:53:38 -0300

    Eliminação da rolagem horizontal

--- Front End/Front End/FrontEnd Ecommerce/home/javascript/listar.js
+++ Front End/Front End/FrontEnd Ecommerce/home/javascript/listar.js
@@ -1,18 +1,30 @@
+// Captura o hash da URL, removendo o símbolo #, e converte para minúsculas
+const categoriaFiltrada = window.location.hash ? window.location.hash.substring(1).toLowerCase() : null;
+
+// Faz a requisição à API
 fetch('http://localhost:8080/livro')
-.then(response => response.json())
-.then(data => {
-    const produtosDiv = document.getElementById('produtos');
-    data.content.forEach(livro => {
-        const div = document.createElement('div');
-        div.innerHTML = `
-            <a href="detalhe-produto.html?id=${livro.id}">
-                <img src="${livro.primeiraImagem}" alt="${livro.titulo}">
-            </a>
-            <h3>${livro.titulo}</h3>
-            <p>${livro.autor}</p>
-            <p>R$ ${livro.preco.toFixed(2)}</p>
-        `;
-        produtosDiv.appendChild(div);
-    });
-})
-.catch(error => console.error('Ocorreu um erro:', error));
+    .then(response => response.json())
+    .then(data => {
+        const produtosDiv = document.getElementById('produtos');
+
+        // Filtra os livros pela categoria se houver uma categoria no hash
+        const livrosFiltrados = categoriaFiltrada
+            ? data.content.filter(livro => livro.categoria.toLowerCase() === categoriaFiltrada)
+            : data.content;
+
+        // Renderiza os livros filtrados
+        livrosFiltrados.forEach(livro => {
+            const div = document.createElement('div');
+            div.innerHTML = `
+                <a href="detalhe-produto.html?id=${livro.id}">
+                    <img src="${livro.primeiraImagem}" alt="${livro.titulo}">
+                </a>
+                <h3>${livro.titulo}</h3>
+                <p>${livro.autor}</p>
+                <p>R$ ${livro.preco.toFixed(2)}</p>
+                <p id=${livro.categoria}></p>
+            `;
+            produtosDiv.appendChild(div);
+        });
+    })
+    .catch(error => console.error('Ocorreu um erro:', error));
